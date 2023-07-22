38db260a160a322628ad3dbdf8cd59fa0897a8f4
yessi30 <104038048+yessi30@users.noreply.github.com>
2023-07-22 19:03:44 -0400

    Crud de alumnos
    Crud de alumnos en php

---
+++ crud-yessica/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => 'arduino1234',
+    'name' => 'tutorial_crud',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
